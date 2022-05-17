edd10656dbf01fbbd7a02c295c271f18859d8888
Beoms <galactic19@naver.com>
2022-05-17 19:14:14 +0900

    tests.py를 사용한 test 하는 기본적 방법1

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,52 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from bs4 import BeautifulSoup
+from .models import *
 
-# Create your tests here.
+
+class TestView(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.no_post = '작성된 게시물이 없습니다.'
+        
+    def test_post_list(self):
+        # 1 포스트 목록 페이지를 가져 온다.
+        post_list = self.client.get('/blog/')
+        
+        # 1-1 정상적으로 페이지가 로드된다.
+        self.assertEqual(post_list.status_code, 200)
+
+        # 1-2 페이지 타이틀은 '블로그 : 목록' 이다.
+        soup = BeautifulSoup(post_list.content, 'html.parser')
+        self.assertEqual(soup.title.text, '블로그 : 목록')
+
+        # 1-3 네비게이션 바가 있다.
+        navbar = soup.nav
+        
+        # 1.4 Blog, About 라는 문구가 네비게이션 바에 있다.
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About', navbar.text)
+        
+        
+        # 2.1 포스트 게시물이 하나도 없다면
+        self.assertEqual(Post.objects.count(), 0)
+        
+        # 2.2 main area에 '작성된 게시물이 없습니다.' 라는 문구가 나타난다.
+        main_area = soup.find('div', id='main-area')
+        self.assertIn(self.no_post, main_area.text)
+        
+        # 3.1 포스트가 2개 있다면
+        post_001 = Post.objects.create(title='첫번째 포스트 입니다', content='Hello World')
+        post_002 = Post.objects.create(title='두번째 포스트 입니다', content='My Test page')
+        self.assertEqual(Post.objects.count(), 2)
+        
+        # 3.2 포스트 목록 페이지를 새로고침 했을 때
+        post_list = self.client.get('/blog/')
+        soup = BeautifulSoup(post_list.content, 'html.parser')
+        self.assertEqual(post_list.status_code, 200)
+        
+        # 3.3 main_area에 포스트 2개의 제목이 존재한다.
+        main_area = soup.find('div', id='main-area')
+        self.assertIn(post_001.title, main_area.text)
+        self.assertIn(post_002.title, main_area.text)
+        
+        self.assertNotIn(self.no_post, main_area.text)
--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('', include('blog.urls')),
+    path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
 ]
 
